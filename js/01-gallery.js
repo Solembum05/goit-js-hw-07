44f8e78973b9e097d913cbaee1c17115a67299b7
Kysil Ivan <kysilivan1997@gmail.com>
2023-04-22 22:24:42 +0200

    make hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+function createGaalery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+const addGalery = createGaalery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', addGalery);
+
+galleryList.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  basicLightbox
+    .create(
+      `<img width="1400" height="900" src="${event.target.dataset.source}">`
+    )
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+function createGaalery(images) {
+  return images
+    .map(({ preview, description, original }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+const addGalery = createGaalery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', addGalery);
+
+let lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
